806a17179d157475171e38654a6ec9d067c13bbd
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-10-21 21:06:08 -0600

    Added news_signup engine
    rails plugin new news_signup --full --mountable -d postgresql --skip-test-unit --dummy-path=spec/dummy

---
+++ engines/news_signup/lib/news_signup/engine.rb
@@ -0,0 +1,5 @@
+module NewsSignup
+  class Engine < ::Rails::Engine
+    isolate_namespace NewsSignup
+  end
+end
---
+++ engines/news_signup/spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '3182d675880121bce16c249117fcd3b6f55f6ecf50d1b39f812ae6056b6ea0628b34d998437fa0131d451c9100a8b3aabc69adaa6bc6a92b433bd07626e030c6'
